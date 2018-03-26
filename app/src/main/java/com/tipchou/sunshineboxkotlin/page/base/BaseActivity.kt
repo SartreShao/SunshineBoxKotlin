bf93e412da9468b8832af8fb24e88699d0230b32
邵励治 <674940850@qq.com>
2018-03-26 21:13:11 +0800

    Initial commit

---
+++ app/src/main/java/com/tipchou/sunshineboxkotlin/page/base/BaseActivity.kt
@@ -0,0 +1,34 @@
+package com.tipchou.sunshineboxkotlin.page.base
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.support.annotation.LayoutRes
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+
+/**
+ * Created by 邵励治 on 2018/3/26.
+ * Perfect Code
+ */
+abstract class BaseActivity : AppCompatActivity() {
+    @LayoutRes
+    protected abstract fun layoutId(): Int
+
+    protected abstract fun created(bundle: Bundle?)
+
+    protected abstract fun resume()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        setContentView(layoutId())
+        supportActionBar?.hide()
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        created(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        resume()
+    }
+}
---
+++ app/src/main/java/com/tipchou/sunshineboxkotlin/page/index/IndexActivity.kt
@@ -0,0 +1,15 @@
+package com.tipchou.sunshineboxkotlin.page.index
+
+import android.os.Bundle
+import com.tipchou.sunshineboxkotlin.R
+import com.tipchou.sunshineboxkotlin.page.base.BaseActivity
+
+class IndexActivity : BaseActivity() {
+    override fun created(bundle: Bundle?) {
+    }
+
+    override fun resume() {
+    }
+
+    override fun layoutId(): Int = R.layout.activity_main
+}
